e77b95b86b3d609965fcaabc68f10b9e6826e0be
InuT <chocolatemozart@gmail.com>
2014-02-12 02:17:24 +0900

    MemoryCache

---
+++ CacheService/CacheService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.Caching;
+
+namespace CacheService
+{
+    class CacheService
+    {
+        private readonly MemoryCache memoryCache;
+
+        private readonly CacheItemPolicy cacheItemPolicy;
+
+        private CacheEntryRemovedCallback callback;
+        
+        public CacheService(string pollingInterval, string limitMegabytes, string limitPercentage, double storingPeriod)
+        {
+            memoryCache = new MemoryCacheGenerator(pollingInterval, limitMegabytes, limitPercentage).MemoryCache;
+
+            callback = new CacheEntryRemovedCallback(this.LoggingRemovedCallback);
+
+            cacheItemPolicy = new CacheItemPolicy();
+            
+            cacheItemPolicy.RemovedCallback = callback; 
+            
+            cacheItemPolicy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(storingPeriod);
+        }
+
+        public void AddCache(string cacheKey, Object item, CacheItemPolicy cacheItemPolicy)
+        {
+            if(!CheckCache(cacheKey))
+            {
+                memoryCache.Add(cacheKey, item, cacheItemPolicy);
+            }
+        }
+
+        public object GetCache(string cacheKey)
+        {
+            if(CheckCache(cacheKey))
+            {
+                return memoryCache.Get(cacheKey);
+            }
+
+            return null;
+        }
+
+        public void RemoveCach(string cacheKey)
+        {
+            if(memoryCache.Contains(cacheKey))
+            {
+                memoryCache.Remove(cacheKey);
+            }
+        }
+
+        private void LoggingRemovedCallback(CacheEntryRemovedArguments arguments)
+        {
+            String log = String.Concat("Reason: ", arguments.RemovedReason.ToString(), 
+                " | Key: ", arguments.CacheItem.Key, " | Value: ", arguments.CacheItem.Value.ToString()); 
+        }
+
+        private Boolean CheckCache(string cacheKey)
+        {
+            if (memoryCache.Contains(cacheKey))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CacheService/MemoryCacheGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Runtime.Caching;
+
+namespace CacheService
+{
+    class MemoryCacheGenerator
+    {
+        private readonly MemoryCache memoryCache;
+
+        private static readonly string MEMORY_CACHE_NAME = "SantaCache";
+
+        public MemoryCacheGenerator(string pollingInterval, string cacheMemoryLimitMegabytes, string physicalMemoryLimitPercentage)
+        {
+            var parameters = new NameValueCollection();
+
+            parameters.Add("pollingInterval", pollingInterval);
+
+            parameters.Add("cacheMemoryLimitMegabytes", cacheMemoryLimitMegabytes);
+
+            parameters.Add("physicalMemoryLimitPercentage", physicalMemoryLimitPercentage);
+
+            memoryCache = new MemoryCache(MEMORY_CACHE_NAME, parameters);
+        }
+
+        public MemoryCache MemoryCache
+        { 
+            get{return memoryCache;} 
+        }
+    }
+}
